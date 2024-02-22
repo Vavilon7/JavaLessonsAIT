205aa01f9b3bb42998188b067bc84a01a6f6a3e3
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-02-22 13:29:22 +0100

    Create JavaModificators.java

---
+++ src/main/java/Lesson11/JavaModificators.java
@@ -0,0 +1,13 @@
+package Lesson11;
+
+public class JavaModificators {
+    int age =20;
+    String name = "Max";
+    String pass = "Deutshland";
+    String colorOfEye = "black";
+    String sex = "male";
+
+    public static void main (String[]args){
+
+    }
+}
