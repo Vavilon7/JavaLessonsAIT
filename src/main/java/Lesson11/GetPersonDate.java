8abcd0820cd7fbadafe52e8ec8d72dbf032e2b33
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-02-24 14:51:25 +0100

    Create GetPersonDate.java

---
+++ src/main/java/Lesson11/GetPersonDate.java
@@ -0,0 +1,12 @@
+package Lesson11;
+
+public class GetPersonDate {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person.getName());
+        System.out.println(person.getColorOfEye());
+        System.out.println(person.getAge());
+
+    }
+}
