1c188f58134892f36d35843a6d39f9e95fa082f2
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-02-24 14:52:07 +0100

    Create Person.java

---
+++ src/main/java/Lesson11/Person.java
@@ -0,0 +1,43 @@
+package Lesson11;
+
+public class Person {
+    private int age = 23;
+
+    private String name = "Max";
+
+    private String pass = "Deutschland";
+
+    private String colorOfEye = "black";
+
+    private String sex = "male";
+
+    private String pincode = "12345";
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getColorOfEye() {
+        return colorOfEye;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+}
+
+
+
+
+
+
+
+
