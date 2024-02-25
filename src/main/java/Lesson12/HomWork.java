de518285c701068697d478207122dc1b5a306345
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-02-25 12:08:31 +0100

    Create HomWork.java

---
+++ src/main/java/Lesson12/HomWork.java
@@ -0,0 +1,25 @@
+package Lesson12;
+
+public class HomWork {
+    /*Напишите программу на Java, которая демонстрирует работу с методами. Ваша программа должна включать
+     метод calculateArea, который принимает в качестве аргументов значения типа double: длину и ширину прямоугольника,
+      и возвращает значение его площади. Также должен быть реализован метод calculatePerimeter, принимающий те же аргументы
+      и возвращающий периметр прямоугольника. Главный метод (main) должен считывать значения длины и ширины, переданные пользователем,
+    вызывать оба эти метода для расчета площади и периметра, и выводить результаты расчетов на экран.
+     */
+    public static void main(String[] args) {
+        double length= 20.55;
+        double width= 30.77;
+
+        double getCalculeteArea=(length + width);
+        System.out.println( " Площадь прямоугольника " + "= " +getCalculeteArea );
+        double calculatePerimeter = 2 * (length + width);
+        System.out.println( " Периметр прямоугольника " + "= " +calculatePerimeter );
+    }
+}
+
+
+
+
+
+
