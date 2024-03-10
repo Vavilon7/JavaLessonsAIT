45536b7d9a302256ebe3d64d77972a332b7f3651
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-03-10 14:25:38 +0100

    Create DogRegistration.java

---
+++ src/main/java/Lesson15/DogRegistration.java
@@ -0,0 +1,10 @@
+package Lesson15;
+
+public class DogRegistration {
+    public static void main(String[] args){
+        Dog dogMops = new Dog("Hatiko","black", "Mops", false,false);
+        System.out.println("Color  " + dogMops.getColor());
+    }
+
+
+}
