efdf79528576248d9130297e8777905b14f3c32c
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-03-30 20:09:44 +0100

    Create TestBook.java

---
+++ src/main/java/Lesson22/TestBook.java
@@ -0,0 +1,22 @@
+package Lesson22;
+
+import java.util.HashSet;
+
+public class TestBook { public static void main(String[] args) {
+    Book pilevinBook = new Book("KGBT+", "Pivevin");
+    Book pilevinNewBook = new Book("KGBT", "Pivevin");
+    Book bookHobbit = new Book("Hobbit", "Tolkien");
+
+   HashSet<Book> bookHashSet = new HashSet<>();
+    bookHashSet.add(pilevinBook);
+    bookHashSet.add(bookHobbit);
+    System.out.println(bookHashSet);
+
+    System.out.println("------");
+    bookHashSet.add(pilevinNewBook);
+    System.out.println(bookHashSet);
+
+
+}
+}
+
