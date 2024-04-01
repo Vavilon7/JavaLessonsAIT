7b6c5422a93c481635845060cf4c0582b03d489f
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-04-01 20:48:06 +0200

    Update AddressDetails.java

--- src/main/java/HomWork22/AddressDetails.java
+++ src/main/java/HomWork22/AddressDetails.java
@@ -8,10 +8,10 @@ import java.util.HashSet;
 public class AddressDetails {
     public static void main(String[] args) {
         HashSet<Property> propertyHashSet = new HashSet<>();
-        Property house = new Property("Victory str. ", "private ", 25);
-        Property apartment = new Property("ul. Tsvetochnaya ", "private ", 127);
-        Property dormitory = new Property("prosp. Stroiteley ", "state property ", 225);
-        Property dormitoryOne= new Property("prosp. Stroiteley ", "state property ", 325);
+        Property house = new Property("Victory str. ", " private ", 25);
+        Property apartment = new Property("ul. Tsvetochnaya ", " private ", 127);
+        Property dormitory = new Property("prosp. Stroiteley ", " state property ", 225);
+        Property dormitoryOne= new Property("prosp. Stroiteley ", " state property ", 325);
         propertyHashSet.add(house);
         propertyHashSet.add(apartment);
         propertyHashSet.add(dormitory);
@@ -34,7 +34,7 @@ public class AddressDetails {
         }
         ArrayList<ArrayList<String>> listOfLists =new ArrayList<>();
         for (Property property : propertyHashSet){
-            System.out.println(property.getAddress());
+            System.out.println(property.getAddress() +property.getNumbers()  + property.getType());
         }
     }
 }
