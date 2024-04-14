da382655896fe01c68e8b7aee9bac9c317b99fba
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-04-14 09:54:43 +0200

    Create OrderStatus.java

---
+++ src/main/java/HomWork26/OrderStatus.java
@@ -0,0 +1,18 @@
+package HomWork26;
+
+public enum OrderStatus {
+    PENDING (" Время приготовления 10 минут"),
+    PROCESSING( " Оформление оплаты по чеку 3 минут"),
+    SHIPPED(" Упаковка еды 4 минуты " ),
+    DELIVERED ( " Время доставки 15 минут"),
+    CANCELLED(" Отмена заказа");
+    private String description;
+    OrderStatus (String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
