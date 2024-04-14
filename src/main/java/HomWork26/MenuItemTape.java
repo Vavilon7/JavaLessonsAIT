0d0142447f1054d17064e01fafcc134b39fcf1f8
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-04-14 09:54:37 +0200

    Create MenuItemTape.java

---
+++ src/main/java/HomWork26/MenuItemTape.java
@@ -0,0 +1,25 @@
+package HomWork26;
+
+public enum MenuItemTape {
+    DRINK("Coca cola", 400  ),
+    DISH("Пицца" , 250  );
+    private String type;
+    private int portion;
+
+
+
+    MenuItemTape(String type, int portion ) {
+        this.type = type;
+        this.portion = portion;
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getPortion() {
+        return portion;
+    }
+}
+
