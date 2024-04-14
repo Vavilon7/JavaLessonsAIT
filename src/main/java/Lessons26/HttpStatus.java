7c3d456bdd4115eec2c72065a6f51559cffa8371
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-04-14 09:55:18 +0200

    Create HttpStatus.java

---
+++ src/main/java/Lessons26/HttpStatus.java
@@ -0,0 +1,22 @@
+package Lessons26;
+
+public enum HttpStatus {
+    OK(200,"OK"),
+    NOT_FOUND(404,"Not found"),
+    INTERNAL_SERVER_ERROR(500,"Internal Server Error");
+    private  final int code;
+    private final String message;
+
+    HttpStatus(int code, String message){
+        this.code = code;
+        this.message =  message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
