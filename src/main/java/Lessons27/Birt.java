b4cb4f0184652eb5b3fd50c8bf2172cb3a64c107
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-04-20 16:18:07 +0200

    Create Birt.java

---
+++ src/main/java/Lessons27/Birt.java
@@ -0,0 +1,11 @@
+package Lessons27;
+
+public class Birt extends Animal {
+    public Birt(String name) {
+        super(name);
+    }
+    @Override
+    public void eat(){
+        System.out.println("I'm bird, I'm hungry");
+    }
+}
