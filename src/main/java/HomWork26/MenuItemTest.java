981732560821f26820dc33ad7014d1fdd48119fa
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-04-20 13:30:27 +0200

    Update MenuItemTest.java

--- src/main/java/HomWork26/MenuItemTest.java
+++ src/main/java/HomWork26/MenuItemTest.java
@@ -2,7 +2,7 @@ package HomWork26;
 
 public class MenuItemTest {
     public static void main(String[] args) {
-        OrderStatus orderStatus= OrderStatus.CANCELLED;
+        OrderStatus orderStatus= OrderStatus.PENDING;
 
          MenuItem pizza = new MenuItem("Pizza",MenuItemTape.DISH);
          MenuItem pelimeni = new MenuItem("Pelimeni",MenuItemTape.DISH);
@@ -11,5 +11,6 @@ public class MenuItemTest {
         System.out.println(pelimeni.toString());
         System.out.println(airan.toString());
         System.out.println(orderStatus.name() + " " + orderStatus.getDescription());
+        System.out.println(orderStatus.canCancel());
    }
-}
+} 
