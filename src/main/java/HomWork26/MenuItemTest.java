6e416bda3ddf5ae43fabe617a28e0042bc8d80b6
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-04-20 13:12:19 +0200

    Create MenuItemTest.java

---
+++ src/main/java/HomWork26/MenuItemTest.java
@@ -0,0 +1,15 @@
+package HomWork26;
+
+public class MenuItemTest {
+    public static void main(String[] args) {
+        OrderStatus orderStatus= OrderStatus.CANCELLED;
+
+         MenuItem pizza = new MenuItem("Pizza",MenuItemTape.DISH);
+         MenuItem pelimeni = new MenuItem("Pelimeni",MenuItemTape.DISH);
+         MenuItem airan = new MenuItem("Airan" , MenuItemTape.DRINK);
+        System.out.println(pizza.toString());
+        System.out.println(pelimeni.toString());
+        System.out.println(airan.toString());
+        System.out.println(orderStatus.name() + " " + orderStatus.getDescription());
+   }
+}
