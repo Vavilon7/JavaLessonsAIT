170448dd22857ba3bf4e638c75c74197183348ba
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-04-21 11:55:49 +0200

    Update Dog.java

--- src/main/java/Lessons27/Dog.java
+++ src/main/java/Lessons27/Dog.java
@@ -2,10 +2,12 @@ package Lessons27;
 
 public class Dog extends Animal {
     public Dog(String name) {
+
         super(name);
     }
 
     public void bark(){
+
         System.out.println(getName() +" barks");
     }
 
