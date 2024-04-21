38e828b2d4ddd3dd42299f5a03936987bb9dff52
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-04-21 11:49:42 +0200

    Update AnimalTest.java

--- src/main/java/Lessons27/AnimalTest.java
+++ src/main/java/Lessons27/AnimalTest.java
@@ -3,11 +3,14 @@ package Lessons27;
 public class AnimalTest {
     public static void main(String[] args) {
         Dog hatiko = new Dog("Hatiko");
-        Cat cat = new Cat("Cat");
-        System.out.println(hatiko.getName());
-        System.out.println(cat.getName());
         hatiko.bark();
+        System.out.println(hatiko.getName());
+
+        Cat cat = new Cat("Cat");
         cat.sayMiau();
+        System.out.println(cat.getName());
+
+
         Birt birt = new Birt ("Fenix");
         birt.eat();
 
