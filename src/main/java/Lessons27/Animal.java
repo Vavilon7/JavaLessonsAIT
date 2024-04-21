bf31d85fd7f46dd45cd63741c5a2085b3a8388b8
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-04-21 11:49:35 +0200

    Update Animal.java

--- src/main/java/Lessons27/Animal.java
+++ src/main/java/Lessons27/Animal.java
@@ -4,14 +4,17 @@ public class Animal {
     private String name;
 
     public Animal(String name) {
+
         this.name = name;
     }
 
     public void eat(){
+
         System.out.println(name + " " + "eats");
     }
 
     public String getName() {
+
         return name;
     }
 }
