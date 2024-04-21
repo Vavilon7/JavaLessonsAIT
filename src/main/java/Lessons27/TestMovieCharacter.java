cfd64a6ebb1d25639fd16d8d5473c618f36cdfad
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-04-21 11:56:14 +0200

    Create TestMovieCharacter.java

---
+++ src/main/java/Lessons27/TestMovieCharacter.java
@@ -0,0 +1,10 @@
+package Lessons27;
+
+public class TestMovieCharacter {
+    public static void main(String[] args) {
+        Hero hero = new Hero("Alex","Superman");
+        hero.speak();
+    }
+}
+
+
