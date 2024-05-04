0d193f160907be7107ea2a0c9f2d5d04993403ad
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-05-04 12:28:25 +0200

    Create FinancialDirector.java

---
+++ src/main/java/HomWork27/FinancialDirector.java
@@ -0,0 +1,7 @@
+package HomWork27;
+
+public class FinancialDirector extends Director {
+    public FinancialDirector(String name, String position, int experienceYears) {
+        super(name, position, experienceYears);
+    }
+}
