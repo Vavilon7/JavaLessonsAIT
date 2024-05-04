c83e428fdf6ad9e2c60da57e06c383d24dfa555a
Vavilon7 <160340471+Vavilon7@users.noreply.github.com>
2024-05-04 12:28:27 +0200

    Update MovieSet.java

--- src/main/java/HomWork27/MovieSet.java
+++ src/main/java/HomWork27/MovieSet.java
@@ -1,19 +1,21 @@
 package HomWork27;
 
+import java.util.ArrayList;
+
 public class MovieSet {
     public static void main(String[] args) {
+        ArrayList<FilmStudioEmployee> filmStudioEmployees = new ArrayList<>();
+        Director director = new Director( "lew", " Direktor", 10);
+        SoundEngineer soundEngineer = new SoundEngineer( "Ivan", "Sound Enginer",6);
+        CameraOperator cameraOperator = new CameraOperator( "Maxim" , " Kameraman", 15);
+        FinancialDirector financialDirector = new FinancialDirector(" Djonson" , "Financial Director ", 5);
 
-        Director misterNic = new Director(" Mister Nic ", " director ", 25);
-        misterNic.leads();
-        System.out.println();
-
-        CameraOperator operator = new CameraOperator(" Ivan ", " operator ", 15);
-        operator.controls();
-        System.out.println();
-
-        SoundEngineer soundEngineer = new SoundEngineer(" Инженер ", " запись ", 12);
-        soundEngineer.recordSound();
-        System.out.println(soundEngineer.getPosition());
-
+        filmStudioEmployees.add(director);
+        filmStudioEmployees.add(soundEngineer);
+        filmStudioEmployees.add(cameraOperator);
+     filmStudioEmployees.add( financialDirector);
+for ( final FilmStudioEmployee filmStudioEmployee: filmStudioEmployees){
+    filmStudioEmployee.work();
+}
     }
 }
